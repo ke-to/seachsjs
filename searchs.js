0854f94e6cfceb6419fdc98b4365a680d7b7fc3f
ke-to <kuronokei0606@gmail.com>
2015-07-19 02:31:05 +0900

    ファイルmerge

--- searchs.js
+++ searchs.js
@@ -1,1 +1,13 @@
-alert("hello");
+javascript:void(
+    s=prompt('Find text:',''));
+    s='(' s ')';x=new RegExp(s,'gi');
+    rn=Math.floor(Math.random()*100);
+    rid='z'+rn;
+    b = document.body.innerHTML;
+    b=b.replace(x,'
+        <span name='+rid+' id='+rid+' style=\'color:#000;
+        background-color:yellow;
+        font-weight:bold;\'>$1</span>');
+    void(document.body.innerHTML=b);
+    alert('Found '+document.getElementsByName(rid).length' matches.');
+    window.scrollTo(0,document.getElementsByName(rid)[0].offsetTop);
