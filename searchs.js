2550c2dfaa79c3a874f68365c70498cac4fbc337
ke-to <kuronokei0606@gmail.com>
2015-07-19 02:32:40 +0900

    Commit1

--- searchs.js
+++ searchs.js
@@ -1,5 +1,6 @@
-javascript:void(
-    s=prompt('Find text:',''));
+javascript:
+    void(s=prompt('Find text:',''));
+
     s='(' s ')';x=new RegExp(s,'gi');
     rn=Math.floor(Math.random()*100);
     rid='z'+rn;
@@ -8,6 +9,7 @@ javascript:void(
         <span name='+rid+' id='+rid+' style=\'color:#000;
         background-color:yellow;
         font-weight:bold;\'>$1</span>');
+
     void(document.body.innerHTML=b);
     alert('Found '+document.getElementsByName(rid).length' matches.');
     window.scrollTo(0,document.getElementsByName(rid)[0].offsetTop);
