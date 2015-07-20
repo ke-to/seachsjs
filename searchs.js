7b7b0b3b3acf2a19a79fd89b439506e8b6d75179
ke-to <kuronokei0606@gmail.com>
2015-07-21 00:41:23 +0900

    2015/07/21

--- searchs.js
+++ searchs.js
@@ -1,21 +1,21 @@
 javascript:
     void(s=prompt('Find text:',''));
     var A = s.split(',');
-
+    var R = [];
     B= A.map(function(d,i){
         d= '('+d+')';
         d= new RegExp(d,'gi');
+        rn=Math.floor(Math.random()*100);
+        R.push(rn);
         return d;
     });
-    rn=Math.floor(Math.random()*100);
-    rid='z'+rn;
+    console.log(R);
     b = document.body.innerHTML;
     B.forEach(function(d,i){
-        b=b.replace(d,'<span name='+rid+' class='+rid+' style=\'color:#000;background-color:yellow;font-weight:bold;\'>$1</span>');
+        b=b.replace(d,'<span name=z'+R[i]+' class=z'+R[i]+' style=\'color:#000;background-color:yellow;font-weight:bold;\'>$1</span>');
     });
-
 
     void(document.body.innerHTML=b);
     alert('Found '+document.getElementsByName(rid).length+' matches.');
 
-    window.scrollTo(0,document.getElementsByName(rid)[0].offsetTop);
+    window.scrollTo(0,document.getElementsByName(R)[0].offsetTop);
